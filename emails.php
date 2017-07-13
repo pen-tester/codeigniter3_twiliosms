e929f1c117dd92356eac52812d34224b19e57f1e
andrew li <andrew.li1987@yandex.com>
2017-07-13 07:10:20 -0500

    email

---
+++ emails.php
@@ -0,0 +1,45 @@
+<?php
+    /*
+     * Enable error reporting
+     */
+
+
+    ini_set( 'display_errors', 0 );
+   // error_reporting( E_ALL );
+ 
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+    $_from="Test";
+    //$_to=$_POST["To"];
+    $_body= "Test body";
+    //$_body= $_POST["Body"];
+
+    /*
+     * Setup email addresses and change it to your own
+     */
+    $from = 'webmaster@sms.probateproject.com';
+    $to = 'andrew.lidev@yandex.com';
+    $subject = 'Sms Received from '.$_from;
+    $message = $_body;
+    $headers = 'From:' . $from;
+ 
+    /*
+     * Test php mail function to see if it returns 'true' or 'false'
+     * Remember that if mail returns true does not guarantee
+     * that you will also receive the email
+     */
+    mail($to,$subject,$message, $headers);
+
+  /*  if(mail($to,$subject,$message, $headers))
+    {
+        echo 'Test email send.';
+    } 
+    else 
+    {
+        echo 'Failed to send.';
+    }*/
+?>
+<Response>
+
+</Response>
